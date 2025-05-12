bb36df8ebc023bea84ad1a57315ea14895413ee6
Dov Farber <dovfarber@gmail.com>
2025-05-12 22:49:14 +0300

    start

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cd /home/ec2-user/parking_ticket_python
+cd /home/bear/parking_ticket_python
 pip install -r requirements.txt
 
 uvicorn app.main:app --host 0.0.0.0 --port 3000
